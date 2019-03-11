2bab4a93cdb407a02da7559788f86becba2731cc
Sansevieria <elizabethleighmather@gmail.com>
2019-03-10 21:29:01 -0500

    functioning...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,21 @@
-
-
-// VARIABLES===============================================
-
-var jolene = ["j", "o", "l", "e", "n", "e"];
-var userGuessLower = userGuess.toLowerCase();
-var userGuessUpper = userGuess.toUpperCase();
-var score = 0;
-var remaining = 7;
-
 // FUNCTIONS =================
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    var jolene = ["j", "o", "l", "e", "n", "e"];
+    var userGuessLower = userGuess.toLowerCase();
+    var userGuessUpper = userGuess.toUpperCase();
+    var score = 0;
+    var remaining = 10;
 
-if (jolene.indexOf(userGuessLower) === -1) {
-    //alert("Nope, try again!");
-    var chances = document.getElementById("remaining").innerHTML = remaining - 1;
-    var lettersUsed = document.getElementById("letters-used").innerHTML = userGuessUpper;
-}
+    if (jolene.indexOf(userGuessLower) === -1) {
+        alert("Nope, try again!");
+        var chances = document.getElementById("remaining").innerHTML = remaining - 1;
+        var lettersUsed = document.getElementById("letters-used").innerHTML = userGuessUpper;
+    }
 
-else {
-    var wins = document.getElementById("wins").innerHTML = score + 1;
-}
+    else {
+        alert("Correct!");
+        var wins = document.getElementById("wins").innerHTML = score + 1;
+    }
+
+};
